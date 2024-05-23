c4d48edbc4369064778a4f47d07e951cf0b2a853
_Emy_3 <68069714+gayanoide@users.noreply.github.com>
2024-05-23 13:45:20 +0200

    Update Menu facture

--- cl.lua
+++ cl.lua
@@ -857,7 +857,7 @@ function OpenBillingMenu()
         local target = GetPlayerServerId(player)
         --print("Avant d'ouvrir le dialogue") -- Debugging line
 
-        local input = exports.ox_lib:inputDialog('Amende L.S.P.D', {
+        local input = exports.ox_lib:inputDialog('Amende L.S.S.D', {
             { type = 'number', label = 'Montant', placeholder = 'Montant de la facture' }
         })
         --print("Après avoir tenté d'ouvrir le dialogue") -- Debugging line
